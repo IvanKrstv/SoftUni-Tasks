7e1a2419cc743d69f5ca8b790b2841c99c55b0f1
Ivan Krastev <igkrastev@gmail.com>
2025-03-16 01:43:26 +0200

    Solved task

--- 11. regex/more_exercise/2. softuni_bar_income.py
+++ 11. regex/more_exercise/2. softuni_bar_income.py
@@ -9,11 +9,14 @@ while True:
         break
 
     match = re.search(pattern, line)
+
     if match:
         customer, product, count, price = match.groups()
         count, price = int(count), float(price)
+
         total_price = count * price
         total_income += total_price
+
         print(f"{customer}: {product} - {total_price:.2f}")
 
 print(f"Total income: {total_income:.2f}")
