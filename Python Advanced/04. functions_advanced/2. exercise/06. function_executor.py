712da342accbfedb7b320aae4d6fdfdd90053346
Ivan Krastev <igkrastev@gmail.com>
2025-05-28 19:35:16 +0300

    Solved task

---
+++ Python Advanced/04. functions_advanced/2. exercise/06. function_executor.py
@@ -0,0 +1,21 @@
+def func_executor(*args):
+    result = ''
+    for arg in args:
+        func = arg[0]
+        func_args = arg[1]
+
+        func_result = func(*func_args)
+
+        result += f'{func.__name__} - {func_result}\n'
+    return result
+
+def sum_numbers(num1, num2):
+    return num1 + num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+print(func_executor(
+    (sum_numbers, (1, 2)),
+    (multiply_numbers, (2, 4))
+))
