2864a437b776d0d80cf21039d11653c8e2f61a73
Ivan Krastev <igkrastev@gmail.com>
2025-06-03 18:32:22 +0300

    Solved task

--- Python Advanced/06. file_handling/2. exercise/1. even_lines.py
+++ Python Advanced/06. file_handling/2. exercise/1. even_lines.py
@@ -4,10 +4,9 @@ path = os.path.join('..', 'ex_files', 'ex_1.txt')
 symbols = {"-", ",", ".", "!", "?"}
 with open(path) as f:
     line_number = 0
-    for line in f:
+    for line_number, line in enumerate(f):
         if line_number % 2 == 0:
             for symbol in symbols:
                 line = line.replace(symbol, '@')
             line_list = line.split()
             print(' '.join(word for word in line_list[::-1]))
-        line_number += 1
