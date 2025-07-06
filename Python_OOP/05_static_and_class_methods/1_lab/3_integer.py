6893925990cc4982047aaac4e051e0193597ba45
Ivan Krastev <igkrastev@gmail.com>
2025-07-06 22:45:09 +0300

    Started solution

---
+++ Python_OOP/05_static_and_class_methods/1_lab/3_integer.py
@@ -0,0 +1,13 @@
+class Integer:
+    def __init__(self, value: int):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value):
+        if not isinstance(float_value, float):
+            return "value is not a float"
+        return Integer(int(float_value))
+
+    @classmethod
+    def from_roman(cls, value):
+        pass
