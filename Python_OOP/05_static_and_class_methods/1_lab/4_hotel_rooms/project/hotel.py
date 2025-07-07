54f6de24e2c6ab4eca02a5cf53db896e11606407
Ivan Krastev <igkrastev@gmail.com>
2025-07-07 20:16:25 +0300

    Reworked task

--- Python_OOP/05_static_and_class_methods/1_lab/4_hotel_rooms/project/hotel.py
+++ Python_OOP/05_static_and_class_methods/1_lab/4_hotel_rooms/project/hotel.py
@@ -4,7 +4,10 @@ class Hotel:
     def __init__(self, name: str):
         self.name = name
         self.rooms: list[Room] = []
-        self.guests = 0
+
+    @property
+    def guests(self):
+        return sum(r.guests for r in self.rooms)
 
     @classmethod
     def from_stars(cls, stars_count: int):
@@ -18,13 +21,40 @@ class Hotel:
         if room:
             room.take_room(people)
 
-
     def free_room(self, room_number: int):
         room = next((r for r in self.rooms if r.number == room_number), None)
         if room:
             room.free_room()
 
     def status(self):
-        return (f"Hotel {self.name} has {sum(r.guests for r in self.rooms)} total guests\n"
+        return (f"Hotel {self.name} has {self.guests} total guests\n"
                 f"Free rooms: {', '.join(str(r.number) for r in self.rooms if not r.is_taken)}\n"
                 f"Taken rooms: {', '.join(str(r.number) for r in self.rooms if r.is_taken)}")
+
+hotel = Hotel.from_stars(5)
+
+
+first_room = Room(1, 3)
+
+second_room = Room(2, 2)
+
+third_room = Room(3, 1)
+
+
+hotel.add_room(first_room)
+
+hotel.add_room(second_room)
+
+hotel.add_room(third_room)
+
+
+hotel.take_room(1, 4)
+
+hotel.take_room(1, 2)
+
+hotel.take_room(3, 1)
+
+hotel.take_room(3, 1)
+
+
+print(hotel.status())
