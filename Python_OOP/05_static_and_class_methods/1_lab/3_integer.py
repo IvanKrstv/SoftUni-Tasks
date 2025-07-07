ad31fe79ab0c00edb5f455f5f4d60da57deb45ce
Ivan Krastev <igkrastev@gmail.com>
2025-07-07 19:53:28 +0300

    Solved task

--- Python_OOP/05_static_and_class_methods/1_lab/3_integer.py
+++ Python_OOP/05_static_and_class_methods/1_lab/3_integer.py
@@ -1,3 +1,11 @@
+roman = {'I':1,
+         'V':5,
+         'X':10,
+         'L':50,
+         'C':100,
+         'D':500,
+         'M':1000}
+
 class Integer:
     def __init__(self, value: int):
         self.value = value
@@ -6,8 +14,37 @@ class Integer:
     def from_float(cls, float_value):
         if not isinstance(float_value, float):
             return "value is not a float"
-        return Integer(int(float_value))
+        return cls(int(float_value))
 
     @classmethod
     def from_roman(cls, value):
-        pass
+        int_value = 0
+        for i in range(len(value)):
+            if i != len(value) - 1 and roman[value[i]] < roman[value[i + 1]]:
+                int_value -= roman[value[i]]
+            else:
+                int_value += roman[value[i]]
+        return cls(int_value)
+
+    @classmethod
+    def from_string(cls, value):
+        if not isinstance(value, str):
+            return "wrong type"
+        try:
+            return cls(int(value))
+        except ValueError:
+            return "wrong type"
+
+first_num = Integer(10)
+
+print(first_num.value)
+
+
+second_num = Integer.from_roman("IV")
+
+print(second_num.value)
+
+
+print(Integer.from_float("2.6"))
+
+print(Integer.from_string(2.6))
