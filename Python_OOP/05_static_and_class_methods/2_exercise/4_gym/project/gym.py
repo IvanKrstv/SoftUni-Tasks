ed0e0cab4815585d754cba49970aa1232cdcc2ac
Ivan Krastev <igkrastev@gmail.com>
2025-07-11 18:17:05 +0300

    Solved task

---
+++ Python_OOP/05_static_and_class_methods/2_exercise/4_gym/project/gym.py
@@ -0,0 +1,67 @@
+from project.customer import Customer
+from project.equipment import Equipment
+from project.exercise_plan import ExercisePlan
+from project.subscription import Subscription
+from project.trainer import Trainer
+
+
+class Gym:
+    def __init__(self):
+        self.customers: list[Customer] = []
+        self.trainers: list[Trainer] = []
+        self.equipment: list[Equipment] = []
+        self.plans: list[ExercisePlan] = []
+        self.subscriptions: list[Subscription] = []
+
+    def add_customer(self, customer: Customer):
+        if customer not in self.customers:
+            self.customers.append(customer)
+
+    def add_trainer(self, trainer: Trainer):
+        if trainer not in self.trainers:
+            self.trainers.append(trainer)
+
+    def add_equipment(self, equipment: Equipment):
+        if equipment not in self.equipment:
+            self.equipment.append(equipment)
+
+    def add_plan(self, plan: ExercisePlan):
+        if plan not in self.plans:
+            self.plans.append(plan)
+
+    def add_subscription(self, subscription: Subscription):
+        if subscription not in self.subscriptions:
+            self.subscriptions.append(subscription)
+
+    def subscription_info(self, subscription_id: int):
+        subscription = self.__find_object(subscription_id, self.subscriptions)
+        customer = self.__find_object(subscription_id, self.customers)
+        trainer = self.__find_object(subscription_id, self.trainers)
+        equipment = self.__find_object(subscription_id, self.equipment)
+        plan = self.__find_object(subscription_id, self.plans)
+
+        info = '\n'.join(repr(o) for o in (subscription, customer, trainer, equipment, plan))
+        return info
+
+    @staticmethod
+    def __find_object(obj_id: int, obj_collection: list):
+        return next((o for o in obj_collection if o.id == obj_id), None)
+
+
+# customer = Customer("John", "Maple Street", "john.smith@gmail.com")
+# equipment = Equipment("Treadmill")
+# trainer = Trainer("Peter")
+# subscription = Subscription("14.05.2020", 1, 1, 1)
+# plan = ExercisePlan(1, 1, 20)
+#
+# gym = Gym()
+#
+# gym.add_customer(customer)
+# gym.add_equipment(equipment)
+# gym.add_trainer(trainer)
+# gym.add_plan(plan)
+# gym.add_subscription(subscription)
+#
+# print(Customer.get_next_id())
+#
+# print(gym.subscription_info(1))
