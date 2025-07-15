a8152d93dc77f34c91ea6080390deb3130d4b8ba
Ivan Krastev <igkrastev@gmail.com>
2025-07-15 18:54:15 +0300

    Solved task

--- Python_OOP/07_solid/2_exercise/01_workers.py
+++ Python_OOP/07_solid/2_exercise/01_workers.py
@@ -1,16 +1,24 @@
-class Worker:
+from abc import ABC, abstractmethod
 
-    def work(self):
+
+class BaseWorker(ABC):
+    @staticmethod
+    @abstractmethod
+    def work():
+        pass
+
+class Worker(BaseWorker):
+    @staticmethod
+    def work():
         print("I'm working!!")
 
 
 class Manager:
-
     def __init__(self):
         self.worker = None
 
     def set_worker(self, worker):
-        assert isinstance(worker, Worker), '`worker` must be of type {}'.format(Worker)
+        assert isinstance(worker, BaseWorker), '`worker` must be of type {}'.format(BaseWorker)
 
         self.worker = worker
 
@@ -18,9 +26,9 @@ class Manager:
         if self.worker is not None:
             self.worker.work()
 
-class SuperWorker:
-
-    def work(self):
+class SuperWorker(BaseWorker):
+    @staticmethod
+    def work():
         print("I work very hard!!!")
 
 
@@ -33,5 +41,7 @@ manager.manage()
 super_worker = SuperWorker()
 try:
     manager.set_worker(super_worker)
+    manager.manage()
 except AssertionError:
     print("manager fails to support super_worker....")
+
--- Python_OOP/07_solid/2_exercise/02_workers_updated.py
+++ Python_OOP/07_solid/2_exercise/02_workers_updated.py
@@ -1,70 +1,94 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 import time
 
-class AbstractWorker:
-    __metaclass__ = ABCMeta
-
+class Workable(ABC):
+    @staticmethod
     @abstractmethod
-    def work(self):
+    def work():
         pass
 
+class Eatable(ABC):
+    @staticmethod
     @abstractmethod
-    def eat(self):
+    def eat():
         pass
 
-class Worker(AbstractWorker):
 
-    def work(self):
+
+class Worker(Workable, Eatable):
+    @staticmethod
+    def work():
         print("I'm normal worker. I'm working.")
-
-    def eat(self):
+    @staticmethod
+    def eat():
         print("Lunch break....(5 secs)")
         time.sleep(5)
 
-class SuperWorker(AbstractWorker):
-
-    def work(self):
+class SuperWorker(Workable, Eatable):
+    @staticmethod
+    def work():
         print("I'm super worker. I work very hard!")
-
-    def eat(self):
+    @staticmethod
+    def eat():
         print("Lunch break....(3 secs)")
         time.sleep(3)
 
 
-class Manager:
 
+class BaseManager(ABC):
     def __init__(self):
         self.worker = None
 
+    @abstractmethod
     def set_worker(self, worker):
-        assert isinstance(worker, AbstractWorker), "`worker` must be of type {}".format(AbstractWorker)
+        pass
+
+
+class WorkManager(BaseManager):
+
+    def set_worker(self, worker):
+        assert isinstance(worker, Workable), "`worker` must be of type {}".format(Workable)
 
         self.worker = worker
 
     def manage(self):
         self.worker.work()
 
+
+class BreakManager(BaseManager):
+    def set_worker(self, worker):
+        assert isinstance(worker, Eatable), "`worker` must be of type {}".format(Eatable)
+
+        self.worker = worker
+
     def lunch_break(self):
         self.worker.eat()
 
-class Robot(AbstractWorker):
 
-    def work(self):
+
+class Robot(Workable):
+    @staticmethod
+    def work():
         print("I'm a robot. I'm working....")
 
-    def eat(self):
-        print("I don't need to eat....")
 
+work_manager = WorkManager()
+break_manager = BreakManager()
+work_manager.set_worker(Worker())
+break_manager.set_worker(Worker())
+work_manager.manage()
+break_manager.lunch_break()
 
-manager = Manager()
-manager.set_worker(Worker())
-manager.manage()
-manager.lunch_break()
+work_manager.set_worker(SuperWorker())
+break_manager.set_worker(SuperWorker())
+work_manager.manage()
+break_manager.lunch_break()
 
-manager.set_worker(SuperWorker())
-manager.manage()
-manager.lunch_break()
+work_manager.set_worker(Robot())
+work_manager.manage()
+try:
+    break_manager.set_worker(Robot())
+    break_manager.lunch_break()
+except:
+    pass
 
-manager.set_worker(Robot())
-manager.manage()
-manager.lunch_break()
