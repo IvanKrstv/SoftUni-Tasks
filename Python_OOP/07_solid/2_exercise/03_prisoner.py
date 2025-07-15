528cf8980b2244d4a66526738ac0f3e2bdc75cc4
Ivan Krastev <igkrastev@gmail.com>
2025-07-15 19:15:15 +0300

    Solved task

--- Python_OOP/07_solid/2_exercise/03_prisoner.py
+++ Python_OOP/07_solid/2_exercise/03_prisoner.py
@@ -5,6 +5,7 @@ class Person:
     def __init__(self, position):
         self.position = position
 
+class FreePerson(Person):
     def walk_north(self, dist):
         self.position[1] += dist
 
@@ -13,12 +14,10 @@ class Person:
 
 
 class Prisoner(Person):
-    PRISON_LOCATION = [3, 3]
+    PRISON_LOCATION = (3, 3)
 
     def __init__(self):
-        super(Prisoner, self).__init__(copy.copy(self.PRISON_LOCATION))
-        self.is_free = False
-
+        super(Prisoner, self).__init__(self.PRISON_LOCATION)
 
 
 prisoner = Prisoner()
