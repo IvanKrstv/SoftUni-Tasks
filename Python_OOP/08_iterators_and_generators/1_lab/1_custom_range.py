3857d3a14da6dd7f1309c5af4632a9593e7c6999
Ivan Krastev <igkrastev@gmail.com>
2025-07-16 18:47:36 +0300

    Reworked tasks

--- Python_OOP/08_iterators_and_generators/1_lab/1_custom_range.py
+++ Python_OOP/08_iterators_and_generators/1_lab/1_custom_range.py
@@ -2,17 +2,23 @@ class custom_range:
     def __init__(self, start: int, end: int):
         self.start = start
         self.end = end
+        self.current = start - 1
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.start <= self.end:
-            temp = self.start
-            self.start += 1
-            return temp
-        else:
+        self.current += 1
+        if self.current > self.end:
             raise StopIteration
+        return self.current
+
+        # if self.start <= self.end:
+        #     temp = self.start
+        #     self.start += 1
+        #     return temp
+        # else:
+        #     raise StopIteration
 
 
 one_to_ten = custom_range(1, 10)
