c1c46a974f87c1d0d30cdb865ebf9b4f83ecfc49
Ivan Krastev <igkrastev@gmail.com>
2025-07-16 17:57:01 +0300

    Solved task

---
+++ Python_OOP/08_iterators_and_generators/1_lab/2_reverse_iter.py
@@ -0,0 +1,18 @@
+class reverse_iter:
+    def __init__(self, iterable):
+        self.iterable = iterable
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.iterable:
+            temp = self.iterable[-1]
+            self.iterable.pop()
+            return temp
+        else:
+            raise StopIteration
+
+reversed_list = reverse_iter([1, 2, 3, 4])
+for item in reversed_list:
+    print(item)
